a2adbfc5fdb3a90345dad7fb958447fb59c55807
Hoz1er <hgx.obsess10n@gmail.com>
2022-05-30 00:07:49 +0800

    添加小班班配色版日历

--- webos/src/main.ts
+++ webos/src/main.ts
@@ -10,12 +10,13 @@ import 'font-awesome/css/font-awesome.min.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 const app = createApp(App)
 app
   .use(store)
   .use(router)
   .use(VueAxios, axios)
-  .use(ElementPlus, { size: 'small', zIndex: 1000 })
+  .use(ElementPlus, { size: 'small', zIndex: 1000, locale: zhCn })
 
 app.mount('#app')
--- webos/src/router/DynamicAppRouter.ts
+++ webos/src/router/DynamicAppRouter.ts
@@ -1,10 +1,15 @@
 import { RouteRecordRaw } from 'vue-router'
 import HelloWorld from '../component/HelloWorld.vue'
+import CalendarComponent from '../component/CalendarComponent.vue'
 
 const DynamicAppRouter: Array<RouteRecordRaw> = [
   {
     path: 'hello',
     component: HelloWorld
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
   }
 ]
 
